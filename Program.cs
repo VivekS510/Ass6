fc22cea67018ed6f841f732c4b1ddbaa12976141
Vivek <svivek1507@gmail.com>
2023-09-21 23:53:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first Value: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Value: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            int sum = first + second;
+            if (sum >= 10 && sum <= 20)
+            {
+                Console.WriteLine("30");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
